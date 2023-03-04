615eb313d0d2a53a13e7f8daf7f29907f52873d5
frixaco <rustam21ashurmatov@gmail.com>
2023-03-04 11:19:19 +0500

    Revert "Fix incorrect `infohash` calculation"
    This reverts commit bbaf175fe4339b222c0872abdc800515590aeb27.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -13,8 +14,6 @@ import (
 	"os"
 	"strconv"
 	"time"
-  "sync"
-  "bytes"
 
 	"frixaco/bitgorrent/bencode"
 )
@@ -25,41 +24,39 @@ type Peer struct {
 }
 
 func (p *Peer) String() string {
-	return p.IP.String() + ":" + strconv.Itoa(int(p.Port)) 
+	return p.IP.String() + ":" + string(rune(p.Port))
 }
 
 func main() {
 	torrentFileLocation := os.Args[1]
-  _ = os.Args[2]
-	// fmt.Printf("Torrent file location: %v\n", torrentFileLocation)
-	// fmt.Printf("Download location: %v\n", downloadLocation)
+	downloadLocation := os.Args[2]
+	fmt.Printf("Torrent file location: %v\n", torrentFileLocation)
+	fmt.Printf("Download location: %v\n", downloadLocation)
 
 	file, _ := ioutil.ReadFile(torrentFileLocation)
 	torrentInfo, err := bencode.Unmarshal(&file)
-  // fmt.Println("TORRENT", torrentInfo)
 	if err != nil {
 		fmt.Println(err)
-    return
 	}
 
 	piecesBytes := torrentInfo.(map[string]interface{})["INFO"].(map[string]interface{})["PIECES"].([]byte)
-	_, err = getPieceHashes(piecesBytes, len(piecesBytes))
+	pieceHashes, err := getPieceHashes(piecesBytes, len(piecesBytes))
 	if err != nil {
 		fmt.Println("error parsing pieces hashes", err)
 		return
 	}
-	// fmt.Println("PIECES", pieceHashes)
+	fmt.Println("PIECES", pieceHashes)
 
 	hashedInfo, infohash, err := bencode.GetInfoHash(&file)
 	if err != nil {
 		fmt.Println("Failed to get infohash")
 	}
-	fmt.Println("INFOHASH", hashedInfo, infohash)
+	fmt.Println("INFOHASH", infohash)
 
-	resp, peerID, err := getTrackerPeers(torrentInfo.(map[string]interface{}), &hashedInfo)
+	resp, _, err := getTrackerPeers(torrentInfo.(map[string]interface{}), &hashedInfo)
 	if err != nil {
-    fmt.Println("error making request", err)
-    return
+		fmt.Println("error making request", err)
+		return
 	}
 	defer resp.Body.Close()
 
@@ -68,7 +65,7 @@ func main() {
 		fmt.Println("error reading body", err)
 		return
 	}
-	// fmt.Println("BODY", string(body))
+	fmt.Println("BODY", string(body))
 
 	tracker, err := bencode.Unmarshal(&body)
 	if err != nil {
@@ -85,106 +82,29 @@ func main() {
 	}
 	fmt.Println("PEERS", peers)
 
-  var wg sync.WaitGroup
-
-  // _, err = getPiece(peers[0], []byte(hashedInfo), peerID)
-  // if err != nil {
-  //   fmt.Println(err)
-  // }
-  for _, peer := range peers {
-    wg.Add(1)
-    go getPiece(peer, []byte(hashedInfo), peerID, &wg)
-  }
-
-  wg.Wait()
+	// downloadedPiece, err := getPiece(peers.([]byte))
+	// if err != nil {
+	// 	fmt.Println("error getting piece")
+	// 	return
+	// }
+	// fmt.Println("downloaded piece", downloadedPiece)
 }
 
-func getPiece(peer Peer, infohash []byte, peerID []byte, wg *sync.WaitGroup) ([]byte, error) {
-	fmt.Println("start getting piece", peer.String())
-  defer wg.Done()
-
-	piece := []byte{}
-
-	conn, err := net.DialTimeout("tcp", peer.String(), 10*time.Second)
-  if err != nil {
-    fmt.Println(err)
-    return nil, err
-  }
-
+func getPiece(peers []byte) ([]byte, error) {
+	peer := string(peers[0:6])
+	conn, err := net.DialTimeout("tcp", peer, 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
 	fmt.Println("conn", conn)
-
-  conn.SetDeadline(time.Now().Add(3 * time.Second))
-	defer conn.SetDeadline(time.Time{}) // Disable the deadline
-
-  pstr := "BitTorrent protocol"
-  buf := make([]byte, len(pstr)+49)
-	buf[0] = byte(len(pstr))
-	curr := 1
-	curr += copy(buf[curr:], pstr)
-	curr += copy(buf[curr:], make([]byte, 8)) // 8 reserved bytes
-	curr += copy(buf[curr:], infohash[:])
-	curr += copy(buf[curr:], peerID[:])
-
-	_, err = conn.Write(buf)
-	if err != nil {
-		return nil, err
-	}
-
-  // response from handshake
-  lengthBuf := make([]byte, 1)
-	_, err = io.ReadFull(conn, lengthBuf)
-	if err != nil {
-		return nil, err
-	}
-	pstrlen := int(lengthBuf[0])
-
-	if pstrlen == 0 {
-		err := fmt.Errorf("pstrlen cannot be 0")
-		return nil, err
-	}
-
-	handshakeBuf := make([]byte, 48+pstrlen)
-	_, err = io.ReadFull(conn, handshakeBuf)
-	if err != nil {
-		return nil, err
-	}
-
-	var infoHash, peerId [20]byte
-
-	copy(infoHash[:], handshakeBuf[pstrlen+8:pstrlen+8+20])
-	copy(peerId[:], handshakeBuf[pstrlen+8+20:])
-
-  infohash2 := infoHash
-  fmt.Println("response", infohash2, infohash)
-
-	if err != nil {
-		return nil, err
-	}
-	if !bytes.Equal(infohash2[:], infohash[:]) {
-		return nil, fmt.Errorf("Expected infohash %x but got %x", infohash2, infohash)
-  }
-
-  buf2 := make([]byte, 4)
-	binary.BigEndian.PutUint32(buf2[0:4], 1)
-  conn.Write(buf2)
-
-  buf3 := make([]byte, 4)
-	binary.BigEndian.PutUint32(buf3[0:4], 2)
-  conn.Write(buf3)
-
-  payload := make([]byte, 12)
-	binary.BigEndian.PutUint32(payload[0:4], uint32(1))
-	binary.BigEndian.PutUint32(payload[4:8], uint32(0))
-	binary.BigEndian.PutUint32(payload[8:12], uint32(16384))
-  fmt.Println("payload", payload)
-
+	piece := []byte{}
 	return piece, nil
 }
 
 func getTrackerPeers(t map[string]interface{}, infohash *string) (*http.Response, []byte, error) {
 	announce := t["ANNOUNCE"].(string)
 	size := t["INFO"].(map[string]interface{})["LENGTH"].(int)
-	peerID := generatePeerID()
+	peerId := generatePeerId()
 
 	base, err := url.Parse(announce)
 	if err != nil {
@@ -193,7 +113,7 @@ func getTrackerPeers(t map[string]interface{}, infohash *string) (*http.Response
 
 	params := url.Values{
 		"info_hash":  []string{*infohash},
-		"peer_id":    []string{string(peerID)},
+		"peer_id":    []string{string(peerId)},
 		"port":       []string{strconv.Itoa(6881)},
 		"uploaded":   []string{"0"},
 		"downloaded": []string{"0"},
@@ -214,13 +134,13 @@ func getTrackerPeers(t map[string]interface{}, infohash *string) (*http.Response
 
 	if err != nil {
 		fmt.Println("Error making GET request", err)
-		return nil, peerID, nil
+		return nil, peerId, nil
 	}
 
-	return resp, peerID, nil
+	return resp, peerId, nil
 }
 
-func generatePeerID() (token []byte) {
+func generatePeerId() (token []byte) {
 	rand.Seed(time.Now().UnixNano())
 	token = make([]byte, 20)
 	rand.Read(token)
@@ -238,19 +158,17 @@ func getPieceHashes(d []byte, l int) ([]string, error) {
 	return pieces, nil
 }
 
-func getPeers(b []byte) ([]Peer, error) {
-  peersCount := len(b) / 6
-  peers := make([]Peer, peersCount)
-
-  for i := 0; i < peersCount; i++ {
-    offset := i * 6
-    maybeIP := net.IP(b[offset:offset+4])
-    if maybeIP == nil {
-      continue
-    }
-    peers[i].IP = maybeIP
-    peers[i].Port = binary.BigEndian.Uint16(b[offset+4:offset+6])
-  }
-
-  return peers, nil
+func getPeers(peersBin []byte) ([]Peer, error) {
+	const peerSize = 6 // 4 for IP, 2 for port
+	numPeers := len(peersBin) / peerSize
+	if len(peersBin)%peerSize != 0 {
+		return nil, errors.New("error getting peers number")
+	}
+	peers := make([]Peer, numPeers)
+	for i := 0; i < numPeers; i++ {
+		offset := i * peerSize
+		peers[i].IP = net.IP(peersBin[offset : offset+4])
+		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4 : offset+6])
+	}
+	return peers, nil
 }
